11ad8170b19f1e0bebf8f0e9a946dbe0a2861ca2
tronPavel <143883351+tronPavel@users.noreply.github.com>
2024-12-22 02:26:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "
-https://tronpavel.github.io/cv"
+  base: "https://tronpavel.github.io/cv",
 })
